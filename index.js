f98ebcc311ea58a3a0d86720513a977eb321da8f
umapatil43100-wq <umapatil43100@gmail.com>
2025-12-11 22:32:46 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div style={{padding:40, fontSize:28}}>
+      <h1>AB Home Realty Website</h1>
+      <p>Demo site setup complete. Full version will load soon.</p>
+    </div>
+  );
+}
